639f52e04f1ae2301b3e8222944e89995427c339
Abubakr Khanooni <abubakrkhanooni20@gmail.com>
2024-07-06 19:34:33 +0530

    saturday eve

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const config=require("./config.json");
 const mongoose=require("mongoose");
 mongoose.connect(config.connectionString)
 
+
+const User=require("./models/user.model")
+
 const express=require("express");
 const cors=require("cors");
 const app=express();
@@ -21,6 +24,81 @@ app.get("/",(req,res)=>{
     res.json({data:"hello"});
 });
 //create account 
-app.post("")
+app.post("/create-account",async (req,res)=>{
+    const {fullName,email,password}=req.body;
+    if(!fullName){
+        return res.status(400).json({error:true,message:"full name is required"});
+    }
+    if(!email){
+        return res.status(400).json({error:true,message:"email is required"});
+    }
+    if(!password){
+        return res.status(400).json({error:true,message:"password is required"});
+    }
+
+    const isUser=await User.findOne({email:email});
+    if(isUser){
+        return res.json({
+            error:true,
+            message:"user already exists"
+        })
+    }
+
+    const user=new User({
+        fullName,
+        email,
+        password
+    });
+
+    await user.save();
+    const accessToken=jwt.sign({user},process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:"36900m"
+    });
+
+    return res.json({
+        error:false,
+        user,
+        accessToken,
+        message:"registration successful"
+    })
+})
+
+
+app.post("/login", async (req,res)=>{
+    const {email,password}=req.body;
+    if(!email){
+        return res.status(400).json({message:"email is required"})
+    }
+    if(!password){
+        return res.status(400).json({message:"password is required"})
+    }
+    const userInfo=await User.findOne({email:email})
+    if(!userInfo){
+        return res.status(400).json({
+            message:"user not found should signup"
+        })
+    }
+    console.log(userInfo);
+    if(userInfo.password===password){
+        const user={user:userInfo}
+        const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:"36000m",
+        })
+
+        return res.json({
+            error:false,
+            message:"login successfull",
+            email,
+            accessToken,
+        })
+    }
+    
+        return res.status(400).json({
+            error:true,
+            message:"invalid credentials"
+        })
+    
+
+})
 app.listen(8000)
 module.exports=app;
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,7 +4,7 @@ const Schema=mongoose.Schema;
 const userSchema=new Schema({
     fullName:{type:String},
     email:{type:String},
-    passwrod:{type:String},
+    password:{type:String},
     createdOn:{type:Date ,default :new Date().getTime()},
 })
 module.exports=mongoose.model("User",userSchema)
